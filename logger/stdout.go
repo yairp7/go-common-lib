f9f3b1e9487b01b68d66b9c7aac5d21682c72990
yairp7 <yairpech@gmail.com>
2024-05-25 00:56:04 +0300

    fix logger

--- logger/stdout.go
+++ logger/stdout.go
@@ -9,17 +9,17 @@ func NewStdoutLogger() *StdoutLogger {
 }
 
 func (l *StdoutLogger) Debug(msg string, args ...any) {
-	fmt.Printf(msg, args...)
+	fmt.Println(fmt.Sprintf(msg, args...))
 }
 
 func (l *StdoutLogger) Info(msg string, args ...any) {
-	fmt.Printf(msg, args...)
+	fmt.Println(fmt.Sprintf(msg, args...))
 }
 
 func (l *StdoutLogger) Warn(msg string, args ...any) {
-	fmt.Printf(msg, args...)
+	fmt.Println(fmt.Sprintf(msg, args...))
 }
 
 func (l *StdoutLogger) Error(msg string, args ...any) {
-	fmt.Printf(msg, args...)
+	fmt.Println(fmt.Sprintf(msg, args...))
 }
