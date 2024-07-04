a79e43c1635b6e88bbf8ed473f30dc3ee9c3aa6d
yairp7 <yairpech@gmail.com>
2024-07-04 10:55:36 +0300

    fix logger

--- logger/stdout.go
+++ logger/stdout.go
@@ -2,24 +2,44 @@ package logger
 
 import "fmt"
 
-type StdoutLogger struct{}
+type StdoutLogger struct {
+	level LogLevel
+}
 
-func NewStdoutLogger() *StdoutLogger {
-	return &StdoutLogger{}
+func NewStdoutLogger(logLevel LogLevel) *StdoutLogger {
+	return &StdoutLogger{
+		level: logLevel,
+	}
 }
 
 func (l *StdoutLogger) Debug(msg string, args ...any) {
+	if l.level > DEBUG {
+		return
+	}
+
 	fmt.Println(fmt.Sprintf(msg, args...))
 }
 
 func (l *StdoutLogger) Info(msg string, args ...any) {
+	if l.level > INFO {
+		return
+	}
+
 	fmt.Println(fmt.Sprintf(msg, args...))
 }
 
 func (l *StdoutLogger) Warn(msg string, args ...any) {
+	if l.level > WARN {
+		return
+	}
+
 	fmt.Println(fmt.Sprintf(msg, args...))
 }
 
 func (l *StdoutLogger) Error(msg string, args ...any) {
+	if l.level > ERROR {
+		return
+	}
+
 	fmt.Println(fmt.Sprintf(msg, args...))
 }
