a527c30a1e6a67eee2a81108f27a486448d0169d
yairp7 <yairpech@gmail.com>
2024-07-18 13:50:52 +0300

    small fix

--- common/fn/fn.go
+++ common/fn/fn.go
@@ -1,15 +1,15 @@
 package fn
 
 type ConditionalReduceAction[T any] struct {
-	condition func(acc T) bool
-	action    func(acc T) T
+	Condition func(acc T) bool
+	Action    func(acc T) T
 }
 
 func ConditionalReduce[T any](initialValue T, crds ...ConditionalReduceAction[T]) T {
 	acc := initialValue
 	for _, crd := range crds {
-		if crd.condition(acc) {
-			acc = crd.action(acc)
+		if crd.Condition(acc) {
+			acc = crd.Action(acc)
 		}
 	}
 	return acc
