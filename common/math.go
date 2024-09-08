ebf20c5991a53feec72b4d61c4257ffea711d6d1
yairp7 <yairpech@gmail.com>
2024-09-08 16:05:11 +0300

    Added Max(), Min()

---
+++ common/math.go
@@ -0,0 +1,19 @@
+package common
+
+type Number interface {
+	int | uint | int8 | uint8 | int16 | uint16 | int32 | uint32 | int64 | uint64 | float32 | float64
+}
+
+func Max[T Number](a, b T) T {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func Min[T Number](a, b T) T {
+	if a < b {
+		return a
+	}
+	return b
+}
