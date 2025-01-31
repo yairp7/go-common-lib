aa41ac60c66da30cb2c83a1578503e6b4326e7ed
yairp7 <yairpech@gmail.com>
2025-01-31 15:35:40 +0200

    small fix

--- ds/orderedmap.go
+++ ds/orderedmap.go
@@ -10,7 +10,7 @@ type OrderedMap[K string, V any] map[K]V
 
 func (m OrderedMap[K, V]) Entries() iter.Seq2[K, V] {
 	return func(yield func(K, V) bool) {
-		for _, k := range m.sortedKeys() {
+		for _, k := range slices.Sorted(maps.Keys(m)) {
 			if !yield(k, m[k]) {
 				return
 			}
